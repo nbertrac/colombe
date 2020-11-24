c0ee89d346c47335b7c359877844c3c20d885ac5
nbertrac <nbertrac@gmail.com>
2020-11-24 16:37:47 +0100

    Sur la branche master  Votre branche est à jour avec 'depot/master'.
     Modifications qui seront validées :
    	modifié :         cp2/Test.js

--- cp2/Test.js
+++ cp2/Test.js
@@ -4,3 +4,29 @@ let b = 6;
 let c = a;
 a = b;
 b = c;
+const resultat = Math.round(Math.random()*10);
+prompt("bienvenus dans mon monde"); 
+number(prompt(Yo)); //change en number//
+const test = Number(prompt("Devine mon nombre"));
+if(guess>resultat) {
+    alert("Trop grand");
+                    } 
+else if(guess<resultat) {
+    alert("Trop petit");
+                        } 
+else if(guess===resultat){
+    alert("Bravo!");
+                        }
+                        // BOUCLE!!//
+const resultat = Math.round(Math.random()*10);
+let guess = Number(prompt("Devine mon nombre"));
+while(guess!==resultat) {
+    guess = Number(prompt("Devine mon nombre"));
+    if(guess>resultat) {
+        alert("Trop grand");
+                        } 
+    else if(guess<resultat) {
+        alert("Trop petit");
+                            } 
+                        }
+alert("Bravo!");
