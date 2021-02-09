178af910558a7d65d1240aeaade1982f1539112f
nbertrac <nbertrac@gmail.com>
2021-02-09 16:41:03 +0100

    dldvbvb2f5

--- cp7/animal.class.php
+++ cp7/animal.class.php
@@ -9,14 +9,74 @@ class Animal{
     private $color='';
     private $weight=0.0;
 
+    //Constantes de class
+    const TYPE_WIND = 'aérien';
+    const TYPE_WATER = 'marin';
+    const TYPE_GROUND = 'terrestre';
+    const TYPE_ELSE = 'autre';
+
+    //Constructeur de la classe
+    public function __construct(string $newName="", string $newType=self::TYPE_ELSE, string $newColor='blanc', float $newWeight=0.02)
+    {
+        //Assigne les valeurs des arguments aux attributs
+        $this->name=$newName;
+        $this->type=$newType;
+        $this->color=$newColor;
+        $this->weight=$newWeight;
+    }
+
     //Accesseurs/Mutateurs (Getters/Setters)
-    public function getType(){
+    public function getType() : string{
         return $this->type;
+    }
+
+    public function setType(string $newType){
+        // LE type ne peut être que : terrestre, marin ou aérien
+        $newType=strtolower($newType);
+        if($newType===self::TYPE_GROUND || $newType===self::TYPE_WATER || $newType===self::TYPE_WIND || $newType===self::TYPE_ELSE) $this->type = $newType;
+        else $this->type = self::TYPE_ELSE;
+    }
+
+    public function getColor() : string{
+        return $this->color;
+    }
+
+    public function setColor(string $newColor){
+        $this->color = $newColor;
+    }
+
+    public function getWeight() : float{
+        return $this->weight;
+    }
+
+    public function setWeight(float $newWeight){
+        // Le poid doit être compris entre 20g(souris) et 1100kg(cheval)
+        if($newWeight>= 0.02 && $newWeight<= 1100) $this->weight = $newWeight;
+        else throw new Exception('Le poid doit être compris entre 20g(souris) et 1100kg(cheval).');
     }
 
     //Méthode publique pour se déplacer
     public function move() : string{
-        return 'Je me déplace';
+        //Selon l'environnement de l'animal afficher le mode de déplacement
+        switch ($this->type) {
+            case self::TYPE_GROUND:
+                return "Je creuse";
+                //break;
+            case self::TYPE_WATER:
+                return "Je nage";
+                //break;
+            case self::TYPE_WIND:
+                return 'Je vole';
+                //break;
+            default:
+               return "Je me teleport";
+        }
+    }
+
+    public function eat(Animal $proie){
+        //Un animal mange un autre animal : la proie perd son poids le prédateur gagne le pois de la proie
+        $this->weight=$this->weight + $proie->getWeight();
+        $proie->weight=0;
     }
 
 }
--- cp7/test_animal.php
+++ cp7/test_animal.php
@@ -6,3 +6,22 @@ $obj1 -> name='Milou'; // Valorisation attribut
 //$obj1->color='Blanc';
 echo '<p>' . $obj1->move(); //Appel méthode
 var_dump($obj1);
+
+echo '<h2>Test 2 : Utilisation des accesseurs</h2>';
+$obj2 = new Animal('joke', Animal::TYPE_WIND, 'blue', 100);
+$obj2->name='Soonpy';
+$obj2->setType('lpol');
+echo '<p>' . $obj2->getType();
+var_dump($obj2);
+
+echo '<h2>Test 3 : Test constante</h2>';
+echo '<p>' . Animal::TYPE_GROUND;
+
+echo '<h2>Test 4 : Test poid, color, move</h2>';
+$obj4 = new Animal('Charizard',Animal::TYPE_WATER ,'red', 700);
+echo '<p>' . $obj4->getWeight(). '    '. $obj4->getColor().'      '. $obj4->move();
+var_dump($obj4);
+
+echo '<h2>Test 4 : Test eat</h2>';
+$obj2->eat($obj4);
+echo '<p>'. $obj2->getWeight().' and   '. $obj4->getWeight();
